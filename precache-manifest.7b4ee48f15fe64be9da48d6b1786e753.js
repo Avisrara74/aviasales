fcf69e0ebcd1d8c0c10aef6a2fd2d8d868ec3aa5
Avisrara74 <avisrara74@gmail.com>
2020-06-15 16:08:29 +0500

    Updates

---
+++ precache-manifest.7b4ee48f15fe64be9da48d6b1786e753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211d4ac9f6b9a254ccab2f0797399b6d",
+    "url": "/JM_aviasales/index.html"
+  },
+  {
+    "revision": "441e7bf9e55084065c7b",
+    "url": "/JM_aviasales/static/css/main.225353b6.chunk.css"
+  },
+  {
+    "revision": "4926be35f74822997c6d",
+    "url": "/JM_aviasales/static/js/2.e3813853.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/JM_aviasales/static/js/2.e3813853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441e7bf9e55084065c7b",
+    "url": "/JM_aviasales/static/js/main.5d3e601a.chunk.js"
+  },
+  {
+    "revision": "1c6c5b9bc574808d9ae6",
+    "url": "/JM_aviasales/static/js/runtime-main.81133d09.js"
+  },
+  {
+    "revision": "d2a4807b0aa4a7ca1e7a82ee730e6fb6",
+    "url": "/JM_aviasales/static/media/main-logo.d2a4807b.png"
+  }
+]);
